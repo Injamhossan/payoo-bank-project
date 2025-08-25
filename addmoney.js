3f4f4db7f32c4ab5b7c76f696bb8a202b03f54b3
Injamhossan <injamhossanmamun@gmail.com>
2025-08-25 17:27:01 +0600

    add money function

--- addmoney.js
+++ addmoney.js
@@ -1,3 +1,5 @@
+
+
 document.getElementById("add_money_btn").addEventListener("click",function(e) {
     e.preventDefault();
     console.log("Add Money Click")
@@ -12,9 +14,30 @@ document.getElementById("add_money_btn").addEventListener("click",function(e) {
 
     const availableBalance = parseInt(document.getElementById("avaible-balance").innerText)
 
+    if(bankaccount.length<11) {
+        alert("Invalid Account Number");
+        return;
+    }
+    // const validPin = 1234
+    // if(pin !== validPin){
+    //     alert("Wrong Pin");
+    //     return;
+    // }
     console.log(availableBalance)
 
     const totalNewAvailableBalance = amount + availableBalance
 
     document.getElementById("avaible-balance").innerText = totalNewAvailableBalance
 })  
+
+
+document.getElementById("add_money_btn").addEventListener("click", function(){
+    document.getElementById("add-money-parent").style.display="block"
+
+    document.getElementById("cashout-button").style.display="none"
+})
+document.getElementById("cash-out-button").addEventListener("click", function(){
+    document.getElementById("add-button").style.display="none"
+
+    document.getElementById("cashout-button").style.display="block"
+})
